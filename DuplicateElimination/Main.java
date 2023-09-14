fc2044211bbd2a9d077aa5c7651fee469726f7c8
quentin damois <quentin.damois@efrei.net>
2023-09-14 11:50:19 -0400

    add the start of the firste exercice

---
+++ DuplicateElimination/Main.java
@@ -0,0 +1,21 @@
+package DuplicateElimination;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] arg) {
+        HashSet<String> firstnameList = new HashSet<>();
+        int nbInputName;
+        System.out.println("Enter the number of firstname you want to input :");
+        Scanner input = new Scanner(System.in);
+        nbInputName = input.nextInt();
+        for(int i = 0; i < nbInputName; i++) {
+            System.out.println("Entrer a firstname:");
+            firstnameList.add(input.nextLine());
+        }
+        System.out.println("Enter a name to see if it is in the set of name :");
+        String name = input.nextLine();
+    }
+}
